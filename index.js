aa2bb472256c8976a6a655581a1080fd2230e9ee
Andrew Jensen <andrewjensen90@gmail.com>
2014-11-15 02:02:16 -0700

    Added MidiKeyboard class, for sending MIDI notes.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+//Include dependencies
+var MidiKeyboard = require("./lib/MidiKeyboard.js");
 var Event = require("./lib/Event.js");
+
+//Include defined events
 var events = require("./events.json");
 
+//Run
+var keyboard = new MidiKeyboard();
+keyboard.connect(1);	//Connect to MIDI port 1
+
 for (var i = 0; i < events.length; i++) {
-	var event = new Event(events[i]);
+	var event = new Event(events[i], keyboard);
 	event.enqueue();
 }
+
+process.on("SIGTERM", function () {
+	console.log("Exiting.");
+	keyboard.disconnect();
+});
